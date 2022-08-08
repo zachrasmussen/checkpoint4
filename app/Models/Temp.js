95fe08acb209d52e9a0794727e18a9ed54a32619
Zach Rasmussen <zsrasmussen@gmail.com>
2022-08-08 06:46:14 -0600

    checkpoint4

--- app/Models/Temp.js
+++ app/Models/Temp.js
@@ -1,20 +1,8 @@
 export class Temp {
   constructor(data) {
-
+    this.clicked = false
     this.name = data.name || ''
     this.temp = data.main.temp || 0
-    this.clicked = false
-  }
-
-  get Template() {
-    return `
-        <div class="col-12 d-flex justify-content-end m-3">
-          <div class="col-4 p-3">
-            <h3 class="text-light text-center">${this.convert} <span>${this.clicked ? "C" : "F"}</span></h3>
-            <h4 class="text-light text-center" onclick="app.tempsController.changeTemp()">${this.name}</h4>
-          </div>
-        </div>
-        `
   }
 
   get convert() {
@@ -27,5 +15,16 @@ export class Temp {
       temp = (temp - 273) * 1.8 + 32;
     }
     return Math.round(temp)
+  }
+
+  get Template() {
+    return `
+        <div class="col-12 d-flex justify-content-end m-3">
+          <div class="col-4 p-3">
+            <h3 class="text-light text-center">${this.convert} <span>${this.clicked ? "C" : "F"}</span></h3>
+            <h4 class="text-light text-center" onclick="app.tempsController.changeTemp()">${this.name}</h4>
+          </div>
+        </div>
+        `
   }
 }
--- app/Services/TempsService.js
+++ app/Services/TempsService.js
@@ -14,6 +14,7 @@ class TempsService {
         ProxyState.temp.clicked = !ProxyState.temp.clicked
         ProxyState.temp = ProxyState.temp
     }
+
 }
 
 export const tempsService = new TempsService()
